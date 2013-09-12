8c6809c85d1ba0094efc46b11f99ad0443b33591
Nic Ferrier <nferrier@ferrier.me.uk>
2013-09-12 22:22:59 +0100

    fix up tests

--- ejit-tests.el
+++ ejit-tests.el
@@ -1,24 +1,5 @@
 ;;; tests for ejit
 
-(ert-deftest ejit/tanslate ()
-  (should
-   (equal
-    (ejit/translate '(MULT(PLUS a (car b)) 2))
-    "MULT(PLUS(a, car(b)), 2)"))
-  (should (equal (ejit/translate '(quote (1 2 3))) "[1, 2, 3]"))
-  (should (equal (ejit-compile '(quote (1 2 3))) "[1, 2, 3]"))
-  ;; not sure about this rule
-  (should (equal (ejit/translate '(quote a)) "\"a\""))
-  (should (equal (ejit-compile '(quote a)) "\"a\""))
-  ;; lists
-  (should (equal (ejit-compile '(list 1 2 3)) "[1, 2, 3]"))
-  (should (equal (ejit-compile '(list 1 (* 2 3) 3))
-                 "[1, MULT(2, 3), 3]"))
-  (should (equal
-           (ejit/translate
-            '(TRYCATCH (PLUS 10 20) (MULT 2 20)))
-           "try { PLUS(10, 20) } catch (e) { MULT(2, 20)}")))
-
 (defun ejit/scratch ()
   "a bunch of scratchy working out stuff."
   (ejit/lisp->ejitlisp
@@ -45,6 +26,25 @@
        (list (FUNCTION (a) (MULT 2)) (MULT 3 4))))
     (print ejit/trace-log (current-buffer))))
 
+(ert-deftest ejit/tanslate ()
+  (should
+   (equal
+    (ejit/translate '(MULT(PLUS a (car b)) 2))
+    "ejit.MULT(ejit.PLUS(a, ejit.car(b)), 2)"))
+  (should (equal (ejit/translate '(quote (1 2 3))) "[1, 2, 3]"))
+  (should (equal (ejit-compile '(quote (1 2 3))) "[1, 2, 3]"))
+  ;; not sure about this rule
+  (should (equal (ejit/translate '(quote a)) "\"a\""))
+  (should (equal (ejit-compile '(quote a)) "\"a\""))
+  ;; lists
+  (should (equal (ejit-compile '(list 1 2 3)) "[1, 2, 3]"))
+  (should (equal (ejit-compile '(list 1 (* 2 3) 3))
+                 "[1, ejit.MULT(2, 3), 3]"))
+  (should (equal
+           (ejit/translate
+            '(TRYCATCH (PLUS 10 20) (MULT 2 20)))
+           "try { ejit.PLUS(10, 20) } catch (e) { ejit.MULT(2, 20)}")))
+
 (ert-deftest ejit-compile ()
   (should
    (equal
@@ -54,7 +54,7 @@
        (let ((a 1)
              (b '(10)))
          (* (+ a (car b)) 2))))
-    "(function (myfunc) { (function (a,b) { MULT(PLUS(a, car(b)), 2) })(1, [10]) })(function (a) { MULT(20, a) })"
+    "(function (myfunc) { (function (a,b) { ejit.MULT(ejit.PLUS(a, ejit.car(b)), 2) })(1, [10]) })(function (a) { ejit.MULT(20, a) })"
     )))
 
 ;;; ejit-tests.el ends here
